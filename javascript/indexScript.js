a3a66609042675d6e8cd50d304f6f9e2e57c81ad
joonjang <w.joon.jang@gmail.com>
2024-02-10 12:19:21 -0800

    changed landing page to simple quote maker

--- javascript/indexScript.js
+++ javascript/indexScript.js
@@ -1,57 +1,75 @@
-const transition = document.querySelector("#bgtransition");
-const textChange = document.querySelector(".textColorChange");
+let quotes = [];
+let currentQuote = {};
+let previousQuote = {};
+let quoteInterval;
 
-const bpTile = document.getElementById("beerpong");
-const influenceTile = document.getElementById("influence");
-const githubTile = document.getElementById("github");
-const codepenTile = document.getElementById("codepen");
-
-var tileHover = false;
-var tileClickColorChange = false;
-
-
-// preload images
-var beerpong = new Image();
-beerpong.src =
-	"https://www.dropbox.com/s/9duz6trkjf6vur5/BPStorePhoto.png?dl=1";
-var influence = new Image();
-influence.src = "https://www.dropbox.com/s/2fh5vcg5seg258g/Influence.png?dl=1";
-var github = new Image();
-github.src = "https://www.dropbox.com/s/l9oksz1oz8d1e9d/github.png?dl=1";
-var codepen = new Image();
-codepen.src = "https://www.dropbox.com/s/u85vyigz7e7w6sh/codepen.png?dl=1";
-
-var topInt;
-
-// start screen color change from black to white
-// background and header text
-window.onscroll = function () {
-	var top = window.pageYOffset;
-	topInt = top;
-	if(!tileHover){
-		if (top >= 50 ) {
-			transition.classList.add("active");
-			textChange.classList.add("active");
-		} else {
-			transition.classList.remove("active");
-			textChange.classList.remove("active");
-		}
-	}
-	
-};
-
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-// contact button collapsible
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    } 
-  });
+function fetchQuotes() {
+    return fetch('quotes.json')
+        .then(response => response.json())
+        .then(data => {
+            quotes = data;
+            updateQuote();
+        })
+        .catch(error => console.error('Error loading quotes:', error));
 }
+
+function getRandomQuote() {
+    if (quotes.length === 0) {
+        return null;
+    }
+    return quotes[Math.floor(Math.random() * quotes.length)];
+}
+
+function displayQuote(quote) {
+    if (!quote) return;
+
+    const quoteTextElement = document.getElementById('quote');
+    // Remove the show class to fade out the current quote text
+    quoteTextElement.classList.remove('show');
+
+    // After a short delay, update the quote text and fade in the new quote text
+    setTimeout(() => {
+        quoteTextElement.textContent = `"${quote.quote}" - ${quote.author}`;
+        previousQuote = currentQuote;
+        currentQuote = quote;
+
+        // Add the show class to fade in the new quote text
+        quoteTextElement.classList.add('show');
+    }, 500); // This delay should match the CSS opacity transition time
+}
+
+
+
+function updateQuote() {
+    const quote = getRandomQuote();
+    if (quote) {
+        displayQuote(quote);
+    }
+}
+
+document.getElementById('new-quote').addEventListener('click', function() {
+    clearInterval(quoteInterval);
+    updateQuote();
+    startAutoUpdate();
+});
+
+document.getElementById('prev-quote').addEventListener('click', function() {
+    if (previousQuote.quote) {
+        displayQuote(previousQuote);
+        clearInterval(quoteInterval);
+        startAutoUpdate();
+    }
+});
+
+function startAutoUpdate() {
+    quoteInterval = setInterval(updateQuote, 10000);
+}
+
+// Combine the window.onload functions
+window.onload = function() {
+    fetchQuotes().then(() => {
+        updateQuote();
+        startAutoUpdate();
+    });
+    document.getElementById('quote').classList.add('show');
+};
